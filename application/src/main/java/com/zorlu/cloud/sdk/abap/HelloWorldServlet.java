c49863a1d3767433f171bbee10eb74bd2ddeabe9
SERKANAKS <SERKANAKS@ZHLEVL15008.mshome.net>
2021-01-11 11:38:18 +0300

    Hello düzeltme

--- application/src/main/java/com/zorlu/cloud/sdk/abap/HelloWorldServlet.java
+++ application/src/main/java/com/zorlu/cloud/sdk/abap/HelloWorldServlet.java
@@ -22,6 +22,6 @@ public class HelloWorldServlet extends HttpServlet
         throws IOException
     {
         logger.info("I am running!");
-        response.getWriter().write("Hello World34!");
+        response.getWriter().write("Hello World3!");
     }
 }
